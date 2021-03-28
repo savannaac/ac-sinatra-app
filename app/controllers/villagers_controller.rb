11d10c83f61dfd5562ef585c90f0878ff04fa385
savanna chiang <savannaac@gmail.com>
2021-03-28 03:56:23 -0500

    removes conditionals from edit, delete routes in villagers controller

--- app/controllers/villagers_controller.rb
+++ app/controllers/villagers_controller.rb
@@ -38,37 +38,28 @@ class VillagersController < ApplicationController
     end
 
     get "/villagers/:id/edit" do
-        @villager = Villager.find_by(params[:id])
+        @villager = Villager.find_by_id(params[:id])
 
-        if logged_in? && @villager.user == current_user
-            erb :"/villagers/edit"
-        else 
-            redirect "/login"
-        end
+        erb :"/villagers/edit"
     end
 
     patch "/villagers/:id" do
         @villager = Villager.find_by_id(params[:id])
 
-        if logged_in? && @villager.user == current_user
-            @villager.name = params([:name])
-            @villager.personality = params([:personality])
-            @villager.species = params([:species])
-            @villager.birthday = params([:birthday])
-            @villager.catchphrase = params([:catchphrase])
+            @villager.name = params[:name]
+            @villager.personality = params[:personality]
+            @villager.species = params[:species]
+            @villager.birthday = params[:birthday]
+            @villager.catchphrase = params[:catchphrase]
             @villager.save
 
-            redirect "/villagers/#{@villager.id}"
-        else
-            redirect "/villagers"
-        end
+            redirect to "/villagers/#{@villager.id}"
     end
 
     delete "/villagers/:id/delete" do
         @villager = Villager.find_by_id(params[:id])
-        if logged_in? && @villager.user_id == current_user.id
-                @villager.destroy
-        end        
-            redirect "/villagers"
+        @villager.delete
+        
+        redirect "/villagers"
     end
 end
