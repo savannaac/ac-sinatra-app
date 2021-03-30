935bdf6c0c2a31b78d867237b6adbf14304ee98e
savanna chiang <savannaac@gmail.com>
2021-03-30 17:20:28 -0500

    creates and renames database

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ ENV['SINATRA_ENV'] ||= "development"
 require "bundler/setup"
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-set :database, { :adapter =>'sqlite3', :database=>'wiki.db' }
+set :database, { :adapter =>'sqlite3', :database =>'friendcrossing.db' }
 
 require "./app/controllers/application_controller"
 require_all "app"
